8ce1a6e1bc288941008b7fc5941bd36ea26b3107
Your Name <sarmed_mahmood@yahoo.com>
2017-04-11 17:20:19 +0300

    this first app of react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="col-lg-12">
+          <h2>Welcome  to the  </h2>
+          <form>
+            <input  className="form-group "type="text" name="name"/>
+            </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
